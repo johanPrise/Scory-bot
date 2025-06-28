05b0cc997f355a963e6e05082acfd2c5729b71ed
Johan Priso <prisojohan2@gmail.com>
2025-06-28 11:04:29 +0100

    Deleted api-server.js

--- src/commands/auth/index.js
+++
@@ -1,7 +0,0 @@
-import start from './start.js';
-import link from './link.js';
-
-export {
-  start,
-  link
-};
--- src/commands/teams/index.js
+++
@@ -1,9 +0,0 @@
-import createTeam from './createTeam.js';
-import addToTeam from './addToTeam.js';
-import getTeamRanking from './getTeamRanking.js';
-
-export {
-  createTeam,
-  addToTeam,
-  getTeamRanking
-};
